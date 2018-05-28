40caecef100ccd6ccc7cdf3c80414c840f48b38e
abdomohamed-pro <32368880+abdomohamed-pro@users.noreply.github.com>
2018-05-28 04:22:13 +0200

    Update acc.js

--- acc.js
+++ acc.js
@@ -2,16 +2,12 @@
 
 window.onload = function()
 {
-  var arr = [https://goo.gl/mkGdJw,
-              ,https://goo.gl/mkGdJw ,
-              https://goo.gl/mkGdJw,
-              "https://goo.gl/mkGdJw" ,
-              "https://goo.gl/mkGdJw" ,
-              "https://goo.gl/mkGdJw" ,
-              "https://goo.gl/mkGdJw ,
-              "https://goo.gl/mkGdJw" ,
-              "https://goo.gl/mkGdJw" ,
-              "https://goo.gl/mkGdJw" ,
+  var arr = ["1-qazokm123987:qazokm",
+              "michaelm1121:puppy3" ,
+              "Guazimodo:gsant98" ,
+              "ESDTfusion:bwl1207" ,
+              "CookedFurby:Cracker" ,
+              "Rollatore:juventus20" ,
 ];
 
 
